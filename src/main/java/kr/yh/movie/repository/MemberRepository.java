beea514b3e61c67153e02f8fdb5df379ea63c657
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-08-25 17:18:50 +0900

    [추가] 회원 리포지토리 개발

---
+++ src/main/java/kr/yh/movie/repository/MemberRepository.java
@@ -0,0 +1,26 @@
+package kr.yh.movie.repository;
+
+import kr.yh.movie.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+    private final EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+}
