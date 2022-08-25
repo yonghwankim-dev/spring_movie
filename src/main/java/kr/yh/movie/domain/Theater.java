048aad4bc94def3ca6fa8a638ac6aed19546c3a1
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-08-25 16:05:49 +0900

    [추가] 상영관 도메인 추가

---
+++ src/main/java/kr/yh/movie/domain/Theater.java
@@ -0,0 +1,25 @@
+package kr.yh.movie.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Theater {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "theater_id")
+    private Long id;
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cinema_id")
+    private Cinema cinema;
+}
