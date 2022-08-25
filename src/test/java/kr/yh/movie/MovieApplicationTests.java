811171e2a7035b0441a3f322f22e61c595f9ce97
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-08-25 15:34:23 +0900

    [추가] 프로젝트 설정 초기화

---
+++ src/test/java/kr/yh/movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.yh.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
