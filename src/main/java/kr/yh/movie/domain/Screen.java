3a44bb9ead51d4ea07a3ee0245f119e4a266af01
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-08-26 16:07:06 +0900

    [추가] 상영 도메인 연관관계 메서드 추가

--- src/main/java/kr/yh/movie/domain/Screen.java
+++ src/main/java/kr/yh/movie/domain/Screen.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -22,6 +24,9 @@ public class Screen {
     private LocalDateTime startDateTime;   // 상영시작시간
     private int round;                      // 상영회차
 
+    @OneToMany(mappedBy = "screen")
+    private List<ScreenSeat> screenSeats = new ArrayList<>();
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "movie_id")
     private Movie movie;
@@ -29,4 +34,21 @@ public class Screen {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "theater_id")
     private Theater theater;
+
+    //== 연관 관계 메서드 ==//
+    public void setMovie(Movie movie){
+        if(this.movie != null){
+            this.movie.getScreens().remove(this);
+        }
+        this.movie = movie;
+        movie.getScreens().add(this);
+    }
+
+    public void setTheater(Theater theater){
+        if(this.theater != null){
+            this.theater.getScreens().remove(this);
+        }
+        this.theater = theater;
+        theater.getScreens().add(this);
+    }
 }
