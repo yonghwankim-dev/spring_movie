6db7a25732bd43453347a018a6c03ba01550dd99
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-08-26 16:06:45 +0900

    [추가] 예매 도메인 연관관계 메서드 추가

--- src/main/java/kr/yh/movie/domain/Reservation.java
+++ src/main/java/kr/yh/movie/domain/Reservation.java
@@ -29,7 +29,25 @@ public class Reservation {
     @JoinColumn(name = "member_id")
     private Member member;    // 회원정보
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "screen_seat_id")
     private ScreenSeat screenSeat;      // 상영좌석정보
+
+    //== 연관 관계 메서드 ==//
+    public void setMember(Member member){
+        if(this.member != null){ // 기존 회원이 존재하는 경우
+            this.member.getReservations().remove(this);
+        }
+        this.member = member;
+        member.getReservations().add(this);
+    }
+
+    public void setScreenSeat(ScreenSeat screenSeat){
+        this.screenSeat = screenSeat;
+        screenSeat.setReservation(this);
+    }
+    //== 생성 메서드 ==//
+    //== 비즈니스 로직 ==//
+    //== 조회 로직 ==//
+
 }
