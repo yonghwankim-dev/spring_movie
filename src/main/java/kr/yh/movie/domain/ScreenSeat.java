9cb5fa7baf2d57b2b28bdea17a502b376db0e3e2
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-08-26 16:07:16 +0900

    [추가] 상영좌석 도메인 연관관계 메서드 추가

--- src/main/java/kr/yh/movie/domain/ScreenSeat.java
+++ src/main/java/kr/yh/movie/domain/ScreenSeat.java
@@ -20,6 +20,10 @@ public class ScreenSeat {
     private Long id;
     private ScreenSeatStatus status;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "screen_id")
     private Screen screen;
@@ -27,4 +31,26 @@ public class ScreenSeat {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "seat_id")
     private Seat seat;
+
+    //== 연관 관계 메서드 ==//
+    public void setReservation(Reservation reservation){
+        this.reservation = reservation;
+    }
+
+    public void setScreen(Screen screen){
+        if(this.screen != null){
+            this.screen.getScreenSeats().remove(this);
+        }
+        this.screen = screen;
+        screen.getScreenSeats().add(this);
+    }
+
+    public void setSeat(Seat seat){
+        if(this.seat != null){
+            this.seat.getScreenSeats().remove(this);
+        }
+        this.seat = seat;
+        seat.getScreenSeats().add(this);
+    }
+
 }
