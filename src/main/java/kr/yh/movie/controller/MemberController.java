23d93aa719b550a4dc3f01e4577f58eda0a79a46
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-08-31 20:29:45 +0900

    [수정] 코드 수정

--- src/main/java/kr/yh/movie/controller/MemberController.java
+++ src/main/java/kr/yh/movie/controller/MemberController.java
@@ -7,10 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,8 +27,14 @@ class MemberController {
     }
 
     @PostMapping("/members/new")
-    public String create(@Valid MemberForm memberForm, BindingResult result){
-        if(result.hasErrors()){
+    public String create(@Valid MemberForm memberForm, Errors errors, Model model){
+        if(errors.hasErrors()){
+            // 회원가입 실패시 데이터 유지
+            model.addAttribute("memberForm", memberForm);
+            Map<String, String> validatorResult = memberService.validateHandling(errors);
+            for(String key : validatorResult.keySet()){
+                model.addAttribute(key, validatorResult.get(key))
+            }
             return "members/createMemberForm";
         }
         Member member = Member.createMember(memberForm);
