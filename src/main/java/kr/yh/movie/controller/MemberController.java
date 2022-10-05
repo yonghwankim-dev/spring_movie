37541486a3ad84cedccd13ec2bd23b742e375242
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-10-05 15:00:31 +0900

    [수정] 회원 컨트롤러 수정

--- src/main/java/kr/yh/movie/controller/MemberController.java
+++ src/main/java/kr/yh/movie/controller/MemberController.java
@@ -14,7 +14,6 @@ import lombok.extern.java.Log;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.WebDataBinder;
@@ -48,29 +47,26 @@ public class MemberController {
 
     @GetMapping("/list")
     public String list(@ModelAttribute("pageVO")PageVO pageVO, Model model){
-        log.info("member list");
         Pageable page = pageVO.makePageable(0, "id");
         Page<Member> result = memberService.findAll(memberService.makePredicates(pageVO.getType(), pageVO.getKeyword()), page);
         model.addAttribute("result", new PageMarker<>(result));
         return "members/list";
     }
 
-    @GetMapping("/new")
-    public String createForm(Model model){
+    @GetMapping("/add")
+    public String addForm(Model model){
         model.addAttribute("memberForm", new MemberForm());
-        return "members/createMemberForm";
+        return "members/add";
     }
 
-    @PostMapping("/new")
-    public String create(@Valid @ModelAttribute MemberForm memberForm, Errors errors, Model model){
+    @PostMapping("/add")
+    public String add(@Valid @ModelAttribute MemberForm memberForm, Errors errors, Model model){
         if(errors.hasErrors()){
-            // 유효성 통과 못한 필드와 메시지를 핸들링
             Map<String, String> validatorResult = memberService.validateHandling(errors);
             for(String key : validatorResult.keySet()){
                 model.addAttribute(key, validatorResult.get(key));
             }
-            // 회원가입 페이지로 다시 리턴
-            return "members/createMemberForm";
+            return "members/add";
         }
 
         Member member = Member.createMember(memberForm);
@@ -80,64 +76,44 @@ public class MemberController {
 
     @GetMapping("/view")
     public String view(Long id, @ModelAttribute("pageVO") PageVO pageVO, Model model){
-        log.info("Member Id : " + id);
-
         memberService.findById(id).ifPresent(member->model.addAttribute("member", member));
         return "members/view";
     }
 
     @GetMapping("/modify")
     public String modifyForm(Long id, @ModelAttribute("pageVO") PageVO pageVO, Model model){
-        log.info("MODIFY ID: " + id);
-
         memberService.findById(id).ifPresent(member->model.addAttribute("form", new MemberForm(member)));
         return "members/modify";
     }
 
     @PostMapping("/modify")
     public String modify(MemberForm form, PageVO pageVO, RedirectAttributes rttr){
-        log.info("Modify member form : " + form);
-
         memberService.findById(form.getId()).ifPresent(origin->{
             origin.changeInfo(form);
-            log.info("after origin.changeInfo : " + origin);
             memberService.save(origin);
             rttr.addFlashAttribute("msg", "success");
             rttr.addAttribute("id", origin.getId());
         });
 
-        // 페이징과 검색했던 결과로 이동하는 경우
         RedirectAttributeUtil.addAttributesPage(pageVO, rttr);
-
         return "redirect:/members/view";
     }
 
     @PostMapping("/delete")
     public String delete(Long id, PageVO pageVO, RedirectAttributes rttr){
-        log.info("DELETE ID : " + id);
-
         memberService.deleteById(id);
 
         rttr.addFlashAttribute("msg", "success");
-
-        // 페이징과 검색했던 결과로 이동하는 경우
         RedirectAttributeUtil.addAttributesPage(pageVO, rttr);
-
         return "redirect:/members/list";
     }
 
     @PostMapping("/deletes")
     public String deletes(@RequestParam(value = "checks") List<Long> ids, PageVO pageVO, RedirectAttributes rttr){
-        log.info("DELETE IDS : " + ids);
-
         memberService.deleteAllById(ids);
 
         rttr.addFlashAttribute("msg", "success");
-
-        // 페이징과 검색했던 결과로 이동하는 경우
         RedirectAttributeUtil.addAttributesPage(pageVO, rttr);
-
         return "redirect:/members/list";
     }
-
 }
