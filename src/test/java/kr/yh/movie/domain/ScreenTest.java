4e86402b0a58684bb723a2391aa816d66e0fdcbe
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-10-19 12:51:29 +0900

    [추가] 상영 서비스 테스트 코드 추가

--- src/test/java/kr/yh/movie/domain/ScreenTest.java
+++ src/test/java/kr/yh/movie/domain/ScreenTest.java
@@ -1,16 +1,18 @@
 package kr.yh.movie.domain;
 
-import kr.yh.movie.service.MovieService;
-import kr.yh.movie.service.ScreenService;
-import kr.yh.movie.service.TheaterService;
+import kr.yh.movie.service.*;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -21,6 +23,11 @@ public class ScreenTest {
     private MovieService movieService;
     @Autowired
     private TheaterService theaterService;
+    @Autowired
+    private SeatService seatService;
+    @Autowired
+    private ScreenSeatService screenSeatService;
+
     @Test
     public void insert(){
         //given
@@ -36,11 +43,38 @@ public class ScreenTest {
                     .movie(movie)
                     .theater(theater)
                     .build();
+            // 상영좌석 생성
+
+
             screenService.save(screen);
 
             startDateTime = startDateTime.plusHours(2);
             round++;
         }
         //then
+    }
+
+    @Test
+    @Transactional
+    public void register(){
+        //given
+        Long movieId = 1L;
+        Long theaterId = 1L;
+        Movie movie = movieService.findById(movieId).get();
+        Theater theater = theaterService.findById(theaterId).get();
+        Screen screen = Screen.builder()
+                              .startDateTime(LocalDateTime.now())
+                              .round(1)
+                              .movie(movie)
+                              .theater(theater)
+                              .build();
+        //when
+        Long screenId = screenService.register(screen);
+        Screen findScreen = screenService.findById(screenId).get();
+        List<ScreenSeat> screenSeats = screenSeatService.findAllByScreenId(screenId);
+        List<Seat> seats = seatService.findAllByTheaterId(theaterId);
+        //then
+        assertThat(findScreen.getId()).isEqualTo(screenId);
+        assertThat(screenSeats.size()).isEqualTo(seats.size());
     }
 }
