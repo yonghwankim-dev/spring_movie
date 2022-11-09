e47bcdcf7de45162b28ed99f309d0bed9d104e26
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-11-09 13:53:49 +0900

    [추가] devtools 설정

---
+++ src/main/java/kr/yh/movie/controller/ticket/TicketController.java
@@ -0,0 +1,18 @@
+package kr.yh.movie.controller.ticket;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@Slf4j
+public class TicketController {
+    @GetMapping("/ticket/depth1")
+    public ModelAndView depth1(){
+        log.info("depth1");
+        ModelAndView mav = new ModelAndView("/ticket/depth1");
+        return mav;
+    }
+}
