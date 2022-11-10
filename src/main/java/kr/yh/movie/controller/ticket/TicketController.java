588be3bb8e3e612dafaf0c6d9360171fa9f2b40e
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-11-10 14:45:47 +0900

    [추가] 예매하기 depth2, 좌석선택 디자인

--- src/main/java/kr/yh/movie/controller/ticket/TicketController.java
+++ src/main/java/kr/yh/movie/controller/ticket/TicketController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @Slf4j
 public class TicketController {
@@ -20,6 +23,17 @@ public class TicketController {
     public ModelAndView depth2(){
         log.info("depth2");
         ModelAndView mav = new ModelAndView("/ticket/depth2");
+
+        mav.getModel().put("seatTitleList", getSeatTitleList());
         return mav;
+    }
+
+    private List<String> getSeatTitleList(){
+        List<String> seatTitleList = new ArrayList<>();
+        for(int i = 65; i <= 75; i++){
+            char alphabet = (char) i;
+            seatTitleList.add(String.valueOf(alphabet));
+        }
+        return seatTitleList;
     }
 }
