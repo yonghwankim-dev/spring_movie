9f5befc47daa6699e646e663647760e0a084f286
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-11-15 22:31:39 +0900

    [수정] 마이페이지 - 메인 메뉴 수정

--- src/main/java/kr/yh/movie/controller/myPage/MyPageController.java
+++ src/main/java/kr/yh/movie/controller/myPage/MyPageController.java
@@ -1,5 +1,6 @@
 package kr.yh.movie.controller.myPage;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,6 +10,16 @@ public class MyPageController {
     @GetMapping("/myPage/myPay/ticketingOrderList")
     public ModelAndView ticketOrderList(){
         ModelAndView mav = new ModelAndView("/myPage/myPay/ticketingOrderList");
+        mav.getModelMap().addAttribute("mTab", 0);
+        mav.getModelMap().addAttribute("sTab", 0);
+        return mav;
+    }
+
+    @GetMapping("/myPage/myInfo/myInfoList")
+    public ModelAndView myInfoList(){
+        ModelAndView mav = new ModelAndView("/myPage/myInfo/myInfoList");
+        mav.getModelMap().addAttribute("mTab", 1);
+        mav.getModelMap().addAttribute("sTab", 0);
         return mav;
     }
 }
