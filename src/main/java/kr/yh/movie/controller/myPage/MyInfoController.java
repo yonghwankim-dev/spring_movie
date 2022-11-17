162162bcf64b7db19f24290c2a3d4254f637b8cb
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-11-17 20:54:56 +0900

    [추가] 마이페이지 - MY정보관리 - 회원탈퇴 페이지 추가

--- src/main/java/kr/yh/movie/controller/myPage/MyInfoController.java
+++ src/main/java/kr/yh/movie/controller/myPage/MyInfoController.java
@@ -49,4 +49,66 @@ public class MyInfoController {
         ModelAndView mav = new ModelAndView("/myPage/myInfo/myInfo_change/depth3");
         return mav;
     }
+
+    @GetMapping("/changeMyPassword/depth1")
+    public ModelAndView changeMyPwdDepth1Get(){
+        ModelAndView mav = new ModelAndView("/myPage/myInfo/mypassword_change/depth1");
+        return mav;
+    }
+
+    @PostMapping("/changeMyPassword/depth1")
+    public ModelAndView changeMyPwdDepth1Post(){
+        ModelAndView mav = new ModelAndView("/myPage/myInfo/mypassword_change/depth2");
+        return mav;
+    }
+
+    @GetMapping("/changeMyPassword/depth2")
+    public ModelAndView changeMyPwdDepth2Get(){
+        ModelAndView mav = new ModelAndView("/myPage/myInfo/mypassword_change/depth2");
+        mav.getModelMap().addAttribute("form", new MemberDTO());
+        return mav;
+    }
+
+    @PostMapping("/changeMyPassword/depth2")
+    public ModelAndView changeMyPwdDepth2Post(){
+        ModelAndView mav = new ModelAndView("/myPage/myInfo/mypassword_change/depth3");
+        return mav;
+    }
+
+    @GetMapping("/changeMyPassword/depth3")
+    public ModelAndView changeMyPwdDepth3Get(){
+        ModelAndView mav = new ModelAndView("/myPage/myInfo/mypassword_change/depth3");
+        return mav;
+    }
+
+    @GetMapping("/myInfoSecession/depth1")
+    public ModelAndView deleteMemberDepth1Get(){
+        ModelAndView mav = new ModelAndView("/myPage/myInfo/myInfo_secession/depth1");
+        return mav;
+    }
+
+    @PostMapping("/myInfoSecession/depth1")
+    public ModelAndView deleteMemberDepth1Post(){
+        ModelAndView mav = new ModelAndView("/myPage/myInfo/myInfo_secession/depth2");
+        return mav;
+    }
+
+    @GetMapping("/myInfoSecession/depth2")
+    public ModelAndView deleteMemberDepth2Get(){
+        ModelAndView mav = new ModelAndView("/myPage/myInfo/myInfo_secession/depth2");
+        mav.getModelMap().addAttribute("form", new MemberDTO());
+        return mav;
+    }
+
+    @PostMapping("/myInfoSecession/depth2")
+    public ModelAndView deleteMemberDepth2Post(){
+        ModelAndView mav = new ModelAndView("/myPage/myInfo/myInfo_secession/depth3");
+        return mav;
+    }
+
+    @GetMapping("/myInfoSecession/depth3")
+    public ModelAndView deleteMemberDepth3Get(){
+        ModelAndView mav = new ModelAndView("/myPage/myInfo/myInfo_secession/depth3");
+        return mav;
+    }
 }
