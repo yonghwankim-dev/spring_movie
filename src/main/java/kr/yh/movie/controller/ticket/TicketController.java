601b1d8e4147599e4b3a6746cbc63f88dcbed47a
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-11-21 15:35:34 +0900

    [추가] TicketController 테스트 코드 추가

--- src/main/java/kr/yh/movie/controller/ticket/TicketController.java
+++ src/main/java/kr/yh/movie/controller/ticket/TicketController.java
@@ -1,5 +1,8 @@
 package kr.yh.movie.controller.ticket;
 
+import kr.yh.movie.domain.Cinema;
+import kr.yh.movie.service.CinemaService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +13,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @Slf4j
 public class TicketController {
+    private final CinemaService cinemaService;
+
     @GetMapping("/ticket/depth1")
     public ModelAndView depth1(){
-        log.info("depth1");
         ModelAndView mav = new ModelAndView("/ticket/depth1");
+        List<Cinema> cinemas = cinemaService.findAll();
+        mav.getModelMap().addAttribute("cinemas", cinemas);
         return mav;
     }
 
--- src/test/java/kr/yh/movie/controller/ticket/TicketControllerTest.java
+++ src/test/java/kr/yh/movie/controller/ticket/TicketControllerTest.java
@@ -1,46 +1,69 @@
 package kr.yh.movie.controller.ticket;
 
+import kr.yh.movie.domain.Cinema;
+import kr.yh.movie.service.CinemaService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = TicketController.class,
+        excludeFilters = @ComponentScan.Filter(type= FilterType.REGEX, pattern = "kr.yh.movie.controller.converter.*"))
 public class TicketControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    private CinemaService cinemaService;
+
     @Test
+    @WithMockUser
     public void testDepth1() throws Exception {
-        this.mockMvc.perform(get("/ticket/depth1")
-                        .accept(TEXT_HTML))
-                .andExpect(status().isOk())
-                .andExpect(view().name("/ticket/depth1"));
+        //given
+        Cinema fakeCinema = createCinemaEntity(1L, "가산디지털", "서울");
+        List<Cinema> fakeCinemas = List.of(fakeCinema);
+
+        //mocking
+        Mockito.when(cinemaService.findAll()).thenReturn(fakeCinemas);
+
+        //when
+        List<Cinema> cinemas = (List<Cinema>)
+                        this.mockMvc.perform(get("/ticket/depth1"))
+                                    .andExpect(status().isOk())
+                                    .andExpect(view().name("/ticket/depth1"))
+                                    .andExpect(model().attributeExists("cinemas"))
+                                    .andReturn().getModelAndView().getModelMap().getAttribute("cinemas");
+
+        //then
+        assertThat(cinemas).isEqualTo(fakeCinemas);
     }
 
-    @Test
-    public void testDepth2() throws Exception {
-        this.mockMvc.perform(get("/ticket/depth2")
-                        .accept(TEXT_HTML))
-                .andExpect(status().isOk())
-                .andExpect(view().name("/ticket/depth2"));
+    private Cinema createCinemaEntity(long id, String name, String location) {
+        return Cinema.builder()
+                .id(id)
+                .name(name)
+                .location(location)
+                .build();
     }
 
-    @Test
-    public void testDepth3() throws Exception {
-        this.mockMvc.perform(get("/ticket/depth3")
-                        .accept(TEXT_HTML))
-                .andExpect(status().isOk())
-                .andExpect(view().name("/ticket/depth3"));
-    }
 }
