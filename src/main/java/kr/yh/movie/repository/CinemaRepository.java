e88cb76c227ac07f55061b16abaac7fff9de6979
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-11-22 16:17:35 +0900

    [추가] 지역별 영화관수 쿼리 추가 및 테스트 코드 추가

--- src/main/java/kr/yh/movie/repository/CinemaRepository.java
+++ src/main/java/kr/yh/movie/repository/CinemaRepository.java
@@ -2,14 +2,18 @@ package kr.yh.movie.repository;
 
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
+import kr.yh.movie.controller.cinema.CinemaLocationDTO;
 import kr.yh.movie.domain.Cinema;
 import kr.yh.movie.domain.Movie;
 import kr.yh.movie.domain.QCinema;
 import kr.yh.movie.domain.QMovie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 public interface CinemaRepository  extends JpaRepository<Cinema, Long>, QuerydslPredicateExecutor<Cinema> {
     default Predicate makePredicates(String type, String keyword){
@@ -33,4 +37,17 @@ public interface CinemaRepository  extends JpaRepository<Cinema, Long>, Querydsl
 
         return builder;
     }
+
+    @Query(value = "select new kr.yh.movie.controller.cinema.CinemaLocationDTO(c.location, count(c))" +
+            "from Cinema c " +
+            "group by c.location " +
+            "order by case when c.location = '서울' then 1 " +
+            "when c.location = '경기/인천' then 2 " +
+            "when c.location = '충청/대전' then 3 " +
+            "when c.location = '전라/광주' then 4 " +
+            "when c.location = '경북/대구' then 5 " +
+            "when c.location = '경남/부산/울산' then 6 " +
+            "when c.location = '강원' then 7 " +
+            "else 8 end")
+    List<CinemaLocationDTO> findAllLocationAndCountGroupByLocation();
 }
--- src/test/java/kr/yh/movie/repository/CinemaRepositoryTest.java
+++ src/test/java/kr/yh/movie/repository/CinemaRepositoryTest.java
@@ -1,6 +1,8 @@
 package kr.yh.movie.repository;
 
+import kr.yh.movie.controller.cinema.CinemaLocationDTO;
 import kr.yh.movie.domain.Cinema;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -14,6 +16,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -56,6 +59,34 @@ public class CinemaRepositoryTest {
                      .name(name)
                      .location(location)
                      .build();
+    }
+
+    @Test
+    public void testFindAllLocationAndCountGroupByLocation(){
+        //given
+        String location = "서울";
+        //when
+        List<CinemaLocationDTO> cinemaLocationDTOS = cinemaRepository.findAllLocationAndCountGroupByLocation();
+        CinemaLocationDTO actual = cinemaLocationDTOS.stream().filter(c -> c.getLocation().equals(location)).findFirst().get();
+        //then
+        assertThat(actual.getLocation()).isEqualTo(location);
+        assertThat(actual.getCount()).isEqualTo(23);
+    }
+
+    @Test
+    public void testFindAllLocationAndCountGroupByLocation_particularOrder(){
+        //given
+        String[] expected = {"서울", "경기/인천", "충청/대전", "전라/광주", "경북/대구", "경남/부산/울산", "강원", "제주"};
+        //when
+        List<CinemaLocationDTO> cinemaLocationDTOS = cinemaRepository.findAllLocationAndCountGroupByLocation();
+        String[] actual = cinemaLocationDTOS.stream()
+                                            .map(CinemaLocationDTO::getLocation)
+                                            .collect(Collectors.joining(" "))
+                                            .split(" ");
+        //then
+        assertThat(actual).isEqualTo(expected);
+
+
     }
 
 }
